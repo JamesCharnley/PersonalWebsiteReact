42e7c4ddca95e80a3fa2536c532dc3dac0b7e99c
JamesCharnley <james.anthony.charnley@gmail.com>
2024-08-20 23:56:07 +1200

    .

---
+++ api/Data/Migrations/20240820115536_dtoupdate.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace api.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class dtoupdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
